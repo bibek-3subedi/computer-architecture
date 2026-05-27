ad9bbb0a9d88fde6b87d495126c3fcb2aa972641
bibek-3subedi <bibek.3subedi@gmail.com>
2026-05-27 14:37:09 +0545

    Modified files

--- Lab_3/encoder_4to2.vhd
+++ Lab_3/encoder_4to2.vhd
@@ -1,24 +1,41 @@
-library IEEE ;
+library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity ENCODER_4TO2 is
 port (
-    I : in std_logic_vector (3 downto 0);      -- 4 inputs
-    Y : out std_logic_vector (1 downto 0);     -- 2-bit output
-    V : out std_logic                          -- Valid : '1' if any input active
+    I : in std_logic_vector(3 downto 0);   -- 4 inputs
+    Y : out std_logic_vector(1 downto 0);  -- 2-bit output
+    V : out std_logic                       -- Valid
 );
 end entity ENCODER_4TO2;
 
 architecture Behavioral of ENCODER_4TO2 is
 begin
-    process ( I )
-    begin
+
+process(I)
+begin
+
+    if I = "0000" then
+        Y <= "00";
+        V <= '0';
+
+    elsif I(3) = '1' then
+        Y <= "11";
         V <= '1';
-        if I (3) = '1' then Y <= "11";
-        elsif I (2) = '1' then Y <= "10";
-        elsif I (1) = '1' then Y <= "01";
-        elsif I (0) = '1' then Y <= "00";
-        else Y <= "00"; V <= '0';
-        end if;
-    end process;
+
+    elsif I(2) = '1' then
+        Y <= "10";
+        V <= '1';
+
+    elsif I(1) = '1' then
+        Y <= "01";
+        V <= '1';
+
+    else
+        Y <= "00";
+        V <= '1';
+    end if;
+
+end process;
+
 end architecture Behavioral;
